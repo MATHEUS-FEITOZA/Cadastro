7424deae48654f1e1a959d83f2cd579eafbafbd1
MATHEUS-FEITOZA <matheusgimenez84@gmail.com>
2022-08-29 13:16:56 -0300

    adição de alertas para cep incorreto e btnEnviar

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 let cep = document.getElementById("cep");
+let btnEnviar = document.getElementById("btnEnviar");
 
-const pesquisarCep = async() => {
+const pesquisarCep = async () => {
     let cep = document.getElementById("cep").value;
     let url = `http://viacep.com.br/ws/${cep}/json/`;
     //const promessa =await fetch(url).then(response => response.json).then();
     let promessa = await fetch(url);
     let dados = await promessa.json();
-    if(dados.hasOwnProperty('erro')){
-        alert("CEP nao existe! Preencha Novamente.");
-    }else{
+    if (dados.hasOwnProperty('erro')) {
+        Swal.fire({
+            icon: 'error',
+            title: 'CPF não existe, preencha novamente!',
+            text: '',
+        });
+    } else {
         preencherDados(dados);
     }
 }
@@ -18,20 +23,29 @@ const preencherDados = (dados) => {
     let bairro = document.getElementById("bairro").value = dados.bairro;
     let cidade = document.getElementById("cidade").value = dados.localidade;
     let estado = document.getElementById("estado").value = dados.uf;
-    
+
 }
 
-function formatarCep(e){
+function formatarCep(e) {
 
-    var v= e.target.value.replace(/\D/g,"")                
-    
-    v = v.replace(/^(\d{5})(\d{3})/,"$1-$2") 
-    
+    var v = e.target.value.replace(/\D/g, "")
+
+    v = v.replace(/^(\d{5})(\d{3})/, "$1-$2")
+
     e.target.value = v;
-    
-    }
 
-cep.addEventListener("focusout",pesquisarCep);
+}
+function confirmaFormulario() {
+    Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'Seu cadastro foi enviado!',
+        showConfirmButton: false,
+        timer: 1500
+    })
+}
+btnEnviar.addEventListener("click", confirmaFormulario)
+cep.addEventListener("focusout", pesquisarCep);
 cep.addEventListener("keyup", formatarCep);
 
 
